0c0df2a2795fc12e85387ad2482591599d7c112f
samuel.herrera <samuelherrerafuente@gmail.com>
2021-02-24 00:17:20 -0600

    fixed selecting domain

--- src/test/simple-client.ts
+++ src/test/simple-client.ts
@@ -3,10 +3,10 @@ import { HypClient } from "../client/hypclient";
 
 let c: any = new HypClient({
     client: {
-        // spaceControlUrl: "https://shiburashid.tk"
+        spaceControlUrl: "https://shiburashid.tk"
     },
-    localhost: { port: 5500 }
+    localhost: { port: 8080 }
 });
-setTimeout(() => {
-    exit(0);
-}, 20000);
+//setTimeout(() => {
+//    exit(0);
+//}, 20000);
