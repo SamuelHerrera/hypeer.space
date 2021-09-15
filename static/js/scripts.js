880b8565127b0613dd72d9a23b8fd361265ca816
Samuel Herrera <samuel.herrera@autozone.com>
2021-09-15 18:21:11 -0500

    completely different ui

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,143 +1,78 @@
-/*!
-* Start Bootstrap - Freelancer v6.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-(function ($) {
-    "use strict"; // Start of use strict
+(function () {
+    const btnCont = document.getElementById('menu');
+    const btnBuilder = (btnD) => {
+        return () => {
+            window.open(btnD.link, btnD.label,
+                `resizable=yes, toolbar=no, menubar=no, status=no, 
+            directories=no, width=${btnD.w | 600}, height=${btnD.h | 600}, 
+            left=${btnD.x | 20}, top=${btnD.y | 20}`);
 
-    // Smooth scrolling using anime.js
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ?
-                target :
-                $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                anime({
-                    targets: 'html, body',
-                    scrollTop: target.offset().top - 72,
-                    duration: 1000,
-                    easing: 'easeInOutExpo'
-                });
-                return false;
-            }
-        }
-    });
-
-    // Scroll to top button appear
-    $(document).scroll(function () {
-        var scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
-        }
-    });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function () {
-        $('.navbar-collapse').collapse('hide');
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-        target: '#mainNav',
-        offset: 80
-    });
-
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
         }
     };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
 
-    // Floating label headings for the contact form
-    $(function () {
-        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function () {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function () {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
+    const buttons = [
+        {
+            label: 'Contratar servicios',
+            link: 'https://hypeer.space',
+            handler: (d) => {
+                return () => {
 
+                }
+            }
+        },
+        {
+            label: 'Code server',
+            link: 'https://codeserver.hypeer.space',
+            handler: btnBuilder
+        },
+        {
+            label: 'SSH',
+            link: 'https://ssh.hypeer.space',
+            handler: btnBuilder
+        },
+        {
+            label: 'VNC',
+            link: 'https://vnc.hypeer.space',
+            handler: btnBuilder
+        },
+        {
+            label: 'File server',
+            link: 'https://files.hypeer.space',
+            handler: btnBuilder
+        },
+        {
+            label: 'Ipfs node',
+            link: 'https://ipfs.hypeer.space',
+            handler: btnBuilder
+        },
+        {
+            label: 'Ipfs gateway',
+            link: 'https://gateway.hypeer.space',
+            handler: (btnD) => {
+                return () => {
+                    const ans = prompt("CID");
+                    if (ans != null) {
+                        window.open(`${btnD.link}/ipfs/${ans}`, btnD.label,
+                            `resizable=yes, toolbar=no, menubar=no, status=no, 
+                        directories=no, width=${btnD.w | 600}, height=${btnD.h | 600}, 
+                        left=${btnD.x | 20}, top=${btnD.y | 20}`);
+                    }
 
-        // ---- bootstrapping-code
-        //
-        // so here's the boring part, the three.js initialization
-
-        const hole = document.getElementById('hole');
-
-        const width = hole.offsetWidth;
-        const height = hole.offsetHeight;
-
-        // create renderer-instance, scene, camera and controls
-        // .... renderer
-        const renderer = new THREE.WebGLRenderer({
-            alpha: true,
-            antialias: true
-        });
-        renderer.setSize(width, height);
-        renderer.setClearColor(0x000000);
-        if (renderer.extensions.get('ANGLE_instanced_arrays') === null) {
-            console.error('ANGLE_instanced_arrays not supported');
-        }
-
-        // .... scene
-        const scene = new THREE.Scene();
-
-        // .... camera and controls
-        const camera = new THREE.PerspectiveCamera(
-            60, width / height, 0.1, 5000);
-        // const controls = new THREE.OrbitControls(camera);
-
-        camera.position.set(10, 70, -50);
-        camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-        // .... run demo-code
-        // initialize simulation
-        const update = init(scene, camera);
-        requestAnimationFrame(function loop(time) {
-            // controls.update();
-
-            // update simulation
-            update(performance.now());
-            renderer.render(scene, camera);
-
-            requestAnimationFrame(loop);
-        });
-
-        // .... bind events
-        window.addEventListener('resize', ev => {
-            const hole = document.getElementById('hole');
-
-            const width = hole.offsetWidth;
-            const height = hole.offsetHeight;
-
-
-            renderer.setSize(width, height);
-            camera.aspect = width / height;
-            camera.updateProjectionMatrix();
-        });
-
-        hole.appendChild(renderer.domElement);
-        renderer.setSize(width, height);
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
-
-        stringRandom.init('.el-st');
-    });
-
-})(jQuery); // End of use strict
+                }
+            }
+        },
+        {
+            label: 'Cloudflare teams',
+            link: 'https://dash.teams.cloudflare.com',
+            handler: btnBuilder
+        },
+    ]
+    for (let btnD of buttons) {
+        let span = document.createElement("span");
+        span.appendChild(document.createTextNode(btnD.label));
+        let btn = document.createElement("button");
+        btn.appendChild(span);
+        btn.onclick = btnD.handler(btnD);
+        btnCont.appendChild(btn);
+    }
+})();
