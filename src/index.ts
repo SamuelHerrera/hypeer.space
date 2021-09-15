d7dbd413c1de19593b4a3f42f65785e781b6c0e9
Samuel Herrera <samuel.herrera@autozone.com>
2021-09-14 20:15:43 -0500

    fixed dependencies

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const debug = Debug("index");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8805;
 
 app.use(cors());
 app.use(HypMiddleware.middleware);
